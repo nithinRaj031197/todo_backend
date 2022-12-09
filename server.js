4298fcc762b9b5212ada9f38ad13c0db6c1de841
nithinRaj031197 <nithinraj.official3@gmail.com>
2022-12-09 15:13:08 +0530

    Url updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-app.use("/api", routes);
+app.use("/", routes);
 
 connection();
 const PORT = process.env.SERVER_PORT ?? 5000;
